29d3477b5f6ce0369dc28fe14cca8b58b36f15f3
Yev Vlasenko <yev.vlasenko@gmail.com>
2019-01-27 19:05:19 +0000

    Updates

---
+++ precache-manifest.d203da72a6a155edcfd1bea20d5dabef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cff3c941fb68d2659e5",
+    "url": "/test-cn/static/js/runtime~main.5cff3c94.js"
+  },
+  {
+    "revision": "b680548025fb48cc225e",
+    "url": "/test-cn/static/js/main.b6805480.chunk.js"
+  },
+  {
+    "revision": "353d4b5a3ff7960db685",
+    "url": "/test-cn/static/js/1.353d4b5a.chunk.js"
+  },
+  {
+    "revision": "b680548025fb48cc225e",
+    "url": "/test-cn/static/css/main.93205f86.chunk.css"
+  },
+  {
+    "revision": "3147b3676c56c902a3cb099a32d488a8",
+    "url": "/test-cn/index.html"
+  }
+];
